f8913eecb90055974c5bdf5637c150292009bbcb
ajlaroya <arthur.j.laroya@gmail.com>
2024-02-13 14:08:31 +1100

    cursor chat bubbles

--- components/cursor/CursorChat.tsx
+++ components/cursor/CursorChat.tsx
@@ -8,10 +8,27 @@ const CursorChat = ({
   updateMyPresence,
 }: CursorChatProps) => {
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    // 54:38
+    updateMyPresence({ message: e.target.value });
+    setCursorState({
+      mode: CursorMode.Chat,
+      previousMessage: null,
+      message: e.target.value,
+    });
   };
 
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {};
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      setCursorState({
+        mode: CursorMode.Chat,
+        previousMessage: cursorState.message,
+        message: "",
+      });
+    } else if (e.key === "Escape") {
+      setCursorState({
+        mode: CursorMode.Hidden,
+      });
+    }
+  };
 
   return (
     <div
@@ -20,27 +37,30 @@ const CursorChat = ({
         transform: `translateX(${cursor.x}px) translateY(${cursor.y}px)`,
       }}
     >
-      {/* {cursorState.mode === CursorMode.Chat && ( */}
-      <>
-        <CursorSVG color="#000" />
+      {cursorState.mode === CursorMode.Chat && (
+        <>
+          <CursorSVG color="#000" />
 
-        <div className="absolute left-2 top-5 bg-blue-500 px-4 py-2 text-sm leading-relaxed text-white rounded-[20px]">
-          {cursorState.previousMessage && (
-            <div>{cursorState.previousMessage}</div>
-          )}
-          <input
-            type="text"
-            className="z-10 w-60 border-none bg-transparent text-white placeholder-blue-300 outline-none"
-            autoFocus={true}
-            onChange={handleChange}
-            onKeyDown={handleKeyDown}
-            placeholder={cursorState.previousMessage ? '' : 'Type a message...'}
-            value={cursorState.message}
-            maxLength={50}
-          />
-        </div>
-      </>
-      {/* )} */}
+          <div className="absolute left-2 top-5 bg-blue-500 px-4 py-2 text-sm leading-relaxed text-white rounded-[20px]">
+            {cursorState.previousMessage && (
+              <div>{cursorState.previousMessage}</div>
+            )}
+            <input
+              type="text"
+              className="z-10 w-60 border-none bg-transparent text-white placeholder-blue-300 outline-none"
+              autoFocus={true}
+              onChange={handleChange}
+              onKeyDown={handleKeyDown}
+              placeholder={
+                cursorState.previousMessage ? "" : "Type a message..."
+              }
+              value={cursorState.message}
+              maxLength={50}
+            />
+          </div>
+        </>
+      )}
+      
     </div>
   );
 };
