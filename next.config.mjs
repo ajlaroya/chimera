b2cc96d884b8890aa0fa7f346dc8309979e23453
ajlaroya <arthur.j.laroya@gmail.com>
2024-02-22 14:28:07 +1100

    modify next config

--- next.config.mjs
+++ next.config.mjs
@@ -9,13 +9,17 @@ const nextConfig = {
       },
     ],
   },
-  webpack: (
-    config,
-    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
-  ) => {
-    config.externals.push({ canvas: "commonjs canvas" });
+  webpack: (config) => {
+    config.externals.push({
+      "utf-8-validate": "commonjs utf-8-validate",
+      bufferutil: "commonjs bufferutil",
+      canvas: "commonjs canvas",
+    });
     return config;
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  }
 };
 
 export default nextConfig;
